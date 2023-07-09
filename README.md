0cfc940bd3253a037fcc28964d487ac5ab6e7343
mrrfv <rm-rfv-no-preserve-root@protonmail.com>
2023-07-09 14:46:18 +0200

    Publish code

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# mastodon-scheduled-toot
- Mastodon bot that posts according to a cron-like schedule
+# Mastodon Scheduled Post Bot
+
+This is a Node.js application that uses the Mastodon API to post a toot at a specified time.
+
+## Usage
+
+1. Run `npm install` to install dependencies.
+2. Go to your Mastodon instance preferences and click Development. Create a new application and copy the access token.
+3. Copy `.env.example` to `.env` and fill in the values.
+4. Run `node index.js`.
+
+## Docker
+
+You can also run this bot in a Docker container. To do so, follow step 2 from the usage guide and from use the following command:
+
+```bash
+docker run -d \
+    -e TZ="Europe/Berlin" \
+    -e CRON_SCHEDULE="0 17 * * *" \
+    -e MASTODON_INSTANCE_URL="your mastodon instance url, for example https://mastodon.social" \
+    -e MASTODON_USER_TOKEN="your mastodon access token" \
+    -e MASTODON_STATUS_TEXT="your toot text" \
+    -e MASTODON_STATUS_VISIBILITY="unlisted" \
+    --restart always \
+    --name mastobot \
+    mrrfv/mastodon-scheduled-toot:latest
+```
+
+Change the value of the `TZ` environment variable for scheduling to work properly.
+
+## License
+
+BSD 3-Clause License. See `LICENSE` for more information.
+
+## Donations
+
+If you would like to donate to support this project, you can do so via Liberapay - see my GitHub profile for the link.
---
+++ index.js
@@ -0,0 +1,66 @@
+// Load the node-schedule module to schedule jobs
+import schedule from 'node-schedule'
+
+// Load the dotenv module to access environment variables
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Load the node-fetch module to make HTTP requests
+import fetch from 'node-fetch';
+
+// Check if the required environment variables are present
+const required_env_vars = [
+  "CRON_SCHEDULE",
+  "MASTODON_INSTANCE_URL",
+  "MASTODON_USER_TOKEN",
+  "MASTODON_STATUS_TEXT",
+  "MASTODON_STATUS_VISIBILITY",
+]
+
+for (const env_var of required_env_vars) {
+  if (!process.env[env_var]) {
+    console.error(`Missing environment variable: ${env_var} - the bot cannot continue.`);
+    console.error(`Required environment variables: ${required_env_vars.join(', ')}`);
+    console.error(`Refer to the documentation for more information.`)
+    process.exit(1);
+  }
+}
+
+
+// Define a function that runs the main logic of the app
+async function main() {
+  // Define the status parameters
+  const statusParams = {
+    status: process.env.MASTODON_STATUS_TEXT, // The text content of the status
+    visibility: process.env.MASTODON_STATUS_VISBILITY, // The visibility of the status
+    // Other parameters are optional, see https://docs.joinmastodon.org/methods/statuses/
+  };
+
+  // Define the API endpoint for posting a status
+  const postStatusURL = `${process.env.MASTODON_INSTANCE_URL}/api/v1/statuses`;
+
+  // Make a POST request with node-fetch
+  fetch(postStatusURL, {
+    method: 'POST', // Specify the HTTP method
+    headers: {
+      'Authorization': `Bearer ${process.env.MASTODON_USER_TOKEN}`, // Example: Bearer 1234567890abcdef
+      'Content-Type': 'application/json', // Specify the content type
+    },
+    body: JSON.stringify(statusParams), // Convert the status parameters to JSON string
+  })
+  .then(res => res.json()) // Parse the response as JSON
+  .then(data => {
+    // Do something with the data, such as logging or displaying it
+    console.log(`Toot posted! Check it out at ${data.url}`);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+}
+
+console.log("Bot started! The toot will be posted according to the schedule.");
+console.log(`Your current schedule: ${process.env.CRON_SCHEDULE}`);
+// Set an interval to run the main function according to the schedule
+schedule.scheduleJob(process.env.CRON_SCHEDULE, () => {
+  main();
+})
